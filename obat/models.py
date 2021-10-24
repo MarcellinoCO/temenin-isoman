fbfbef2156a253253094d59e9c6864f359561ccf
Stephanus Dario Kohar <stephanus.dario@ui.ac.id>
2021-10-24 23:26:09 +0700

    third

--- obat/models.py
+++ obat/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Obat(models.Model):
-    penyakit = models.CharField(max_length=30)
-    penjelasan= models.CharField(max_length=100)
-    daftar_obat= models.CharField(max_length=100)
+    penyakit = models.CharField(max_length=100)
+    penjelasan= models.CharField(max_length=1000)
+    daftar_obat= models.CharField(max_length=1000)
--- obat/views.py
+++ obat/views.py
@@ -10,11 +10,43 @@ def index(request):
     obats = Obat.objects.all()
     response = {'obats': obats}
     return render(request, 'obat.html', response)
+    
 @login_required(login_url='/admin/login/?next=/obat/add')
 def add(request):
     obats = Obat.objects.all()
     response = {'obats': obats}
     return render(request, 'form.html', response)
+
+def add_forms(request):
+    context ={}
+    form = ObatForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        if request.method == 'POST':
+            return HttpResponseRedirect("/obat/add")
+    context['form']= form
+    return render(request, "addforms.html", context)
+
+def delete_obat(request):
+    if (request.method == "POST"):
+        id = request.POST.get("id")
+        Obat.objects.filter(id=id).delete()
+    return HttpResponseRedirect("/obat")
+
+def edit_obat(request):
+    if (request.method == "POST"):
+        id = request.POST.get("id")
+        penyakit = request.POST.get("penyakit")
+        penjelasan = request.POST.get("penjelasan")
+        daftar_obat = request.POST.get("daftar_obat")
+
+        obat = Obat.objects.get(id=id)
+        obat.penyakit = penyakit
+        obat.penjelasan = penjelasan
+        obat.daftar_obat = daftar_obat
+        obat.save()
+
+    return HttpResponseRedirect("/obat")
 
 def json(request):
     obats = Obat.objects.all()
