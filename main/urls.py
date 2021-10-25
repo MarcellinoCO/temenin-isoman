ca128f77dee55de619e2d60713a1519c52b14bf8
indramahaarta <i.made023@ui.ac.id>
2021-10-25 18:07:44 +0800

    add authentication including log-in page, sign-up page, log-out, decorators.py for user permissions, etc

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from .views import *
 from . import views
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sign-up/', signup_user, name='sign-up'),
+    path('log-in/', login_user, name='log-in'),
+    path('log-out/', logout_user, name='log-out'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,93 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import CreateUserForm
+from .decorators import *
 
 
+# showing homepage
 def home(request):
     return render(request, 'main/home.html')
+
+
+# handling sign up form and create user with a spesific role (group)
+def signup_user(request):
+    form = CreateUserForm()
+
+    content = {}
+
+    # exexuted when user submit form
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        role = request.POST.get('as')
+
+        # executed when form is valid
+        if form.is_valid():
+
+            # save user and redirect user to login pages
+            form.save()
+
+            # getting user and groups object
+            username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+            common_user = Group.objects.get(name="common_user")
+            fasilitas_kesehatan = Group.objects.get(name="fasilitas_kesehatan")
+
+            # Aadding group
+            if role[0] == 'User':
+                user.groups.add(common_user)
+            else:
+                user.groups.add(fasilitas_kesehatan)
+                user.is_staff = True
+                user.save()
+
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('/log-in/')
+
+        # exexuted when form is not valid
+        else:
+
+            # redirect user back to sign up page
+            messages.success(request, 'Form is not valid. Try again!')
+            return redirect('/sign-up/')
+
+    # render signup.html
+    content['form'] = form
+    return render(request, 'main/signup.html', content)
+
+
+# handling log in form and authenticate someone as a user
+def login_user(request):
+
+    # excecuted when user submiting form
+    if request.method == 'POST':
+
+        # authenticating user based on username and password
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        # executed when user if valid
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+        # excecuted when user is not valid
+        else:
+            messages.success(
+                request, 'There was an error Loging In. Try again!')
+            return redirect('/log-in/')
+
+    # rendering login.html
+    else:
+        return render(request, 'main/login.html', {})
+
+
+# handling log out
+def logout_user(request):
+    logout(request)
+
+    # redirect to mainpage
+    return redirect('/')
