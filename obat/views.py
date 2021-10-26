6328c6a1a66e2957f2de0e1aeac9592655ddba6e
Stephanus Dario Kohar <stephanus.dario@ui.ac.id>
2021-10-26 12:12:21 +0700

    fifth

--- obat/views.py
+++ obat/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import Obat
 from .forms import ObatForm
-from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.http.response import HttpResponse
 # Create your views here.
@@ -11,7 +10,6 @@ def index(request):
     response = {'obats': obats}
     return render(request, 'obat.html', response)
     
-@login_required(login_url='/admin/login/?next=/obat/add')
 def add(request):
     obats = Obat.objects.all()
     response = {'obats': obats}
