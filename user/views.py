d7a6c91725c7bec910d08baba897a13c8ae481ba
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-12-23 01:13:42 +0700

    Update status code for user endpoints

--- user/views.py
+++ user/views.py
@@ -9,45 +9,45 @@ from main.forms import CreateUserForm
 @csrf_exempt
 def sign_in(request):
     if request.method != "POST":
-        return JsonResponse({"result": "Must use POST method!"})
+        return JsonResponse({"result": "Must use POST method!"}, status=405)
 
     if not request.body:
-        return JsonResponse({"result": "Must provide request body!"})
+        return JsonResponse({"result": "Must provide request body!"}, status=400)
 
     username = request.POST['username']
     password = request.POST['password']
 
     if not username or not password:
-        return JsonResponse({"result": "Must provide username and password!"})
+        return JsonResponse({"result": "Must provide username and password!"}, status=400)
 
     user = authenticate(request, username=username, password=password)
 
     if user is not None:
         login(request, user)
-        return JsonResponse({"result": "success"})
+        return JsonResponse({"result": "success"}, status=200)
     else:
-        return JsonResponse({"result": "failed"})
+        return JsonResponse({"result": "failed"}, status=500)
 
 
 @csrf_exempt
 def sign_out(request):
     if request.method != "POST":
-        return JsonResponse({"result": "Must use POST method!"})
+        return JsonResponse({"result": "Must use POST method!"}, status=405)
 
     if not request.user.is_authenticated:
-        return JsonResponse({"result": "Not yet authenticated!"})
+        return JsonResponse({"result": "Not yet authenticated!"}, status=403)
 
     logout(request)
-    return JsonResponse({"result": "success"})
+    return JsonResponse({"result": "success"}, status=200)
 
 
 @csrf_exempt
 def sign_up(request):
     if request.method != "POST":
-        return JsonResponse({"result": "Must use POST method!"})
+        return JsonResponse({"result": "Must use POST method!"}, status=405)
 
     if not request.body:
-        return JsonResponse({"result": "Must provide request body!"})
+        return JsonResponse({"result": "Must provide request body!"}, status=400)
 
     email = request.POST['email']
     username = request.POST['username']
@@ -55,7 +55,7 @@ def sign_up(request):
     role = request.POST['role']
 
     if not email or not username or not password:
-        return JsonResponse({"result": "Must provide email, username, and password"})
+        return JsonResponse({"result": "Must provide email, username, and password"}, status=400)
 
     form = CreateUserForm({
         'username': username,
@@ -78,26 +78,26 @@ def sign_up(request):
         else:
             user.groups.add(common_user)
 
-        return JsonResponse({"result": "Sign up success!"})
+        return JsonResponse({"result": "Sign up success!"}, status=200)
 
-    return JsonResponse({"result": "Sign up data not valid!"})
+    return JsonResponse({"result": "Sign up data not valid!"}, status=400)
 
 
 @csrf_exempt
 def get_dummy(request):
     if request.method != "POST":
-        return JsonResponse({"result": "Must use POST method!"})
+        return JsonResponse({"result": "Must use POST method!"}, status=405)
 
     if not request.user.is_authenticated:
-        return JsonResponse({"data": "unauthorized"})
+        return JsonResponse({"result": "Not yet authenticated!"}, status=403)
 
-    return JsonResponse({"data": "wowowowowoww"})
+    return JsonResponse({"data": "wowowowowoww"}, status=200)
 
 
 @csrf_exempt
 def get_user(request):
     if request.method != "POST":
-        return JsonResponse({"result": "Must use POST method!"})
+        return JsonResponse({"result": "Must use POST method!"}, status=405)
 
     user = request.user
 
