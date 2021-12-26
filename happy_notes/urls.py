477bad7ad270652eff7200ab5a2a33e612cd6dff
Dinda <74523925+dindasrg@users.noreply.github.com>
2021-12-26 23:29:48 +0700

    New happy (#23)
    * modul happy-notes
    
    * finishing happy-notes feature
    
    * migrate
    
    * Update settings.py
    
    * Update urls.py
    
    resolve conflict
    
    * delete all file in migrations except __init__ and 0001_initial
    
    * pull
    
    * nambahin add-from-flutter dan return json
    
    * final check
    
    * Delete 0002_auto_20211105_2341.py
    
    Delete mogrations
    
    * Delete 0002_auto_20211105_2341.py
    
    Delete migrations

--- happy_notes/urls.py
+++ happy_notes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, load_notes_view
+from .views import index, load_notes_view, notes_json, add_from_flutter
 
 urlpatterns = [
     path('', index, name='index'),
 
     path('data/', load_notes_view, name='notes-data'),
+    path('notes-json', notes_json, name='notes-json'),
+    path('add-from-flutter', add_from_flutter, name='add-from-flutter')
 ]
